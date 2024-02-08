1436d62851cf1201779f18d2cb36066f97cb3477
pragatinautiyal <158483355+pragatinautiyal@users.noreply.github.com>
2024-02-08 21:34:21 +0530

    Create First.c

---
+++ First.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+{
+	int a[10],i,t,n,k;
+	printf("Input number of elements in array\n");
+	scanf("%d",&n);
+	printf("Input elements\n");
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	printf("Input k\n");
+	scanf("%d",&k);
+	i=(k<0?0:n-1);
+	if(k>0)
+	{
+		while(k!=0)
+		{
+			t=a[i];
+			a[i]=a[i-1];
+			a[i-1]=t;
+			i--;
+			if(i==0)
+			{
+				k--;
+				i=n-1;
+			}
+		}
+	}
+	else
+	{
+		while(k!=0)
+		{
+		t=a[i];
+		a[i]=a[i+1];
+		a[i+1]=t;
+		i++;
+		if(i==n-1)
+		{
+			k++;
+			i=0;
+		}
+		}
+	}
+	printf("New array is\n");
+	for(i=0;i<n;i++)
+	printf("%d\t",a[i]);
+	return 0;
+}
